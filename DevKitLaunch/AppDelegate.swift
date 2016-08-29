c34f24bc14b3081b0ccadde99b839a14430b487d
Franck Brun <franck.brun@me.com>
2016-08-29 10:43:29 +0200

    Initial Commit

---
+++ DevKitLaunch/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  DevKitLaunch
+//
+//  Created by Franck Brun on 29/08/2016.
+//  Copyright © 2016 Franck Brun. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  @IBOutlet weak var window: NSWindow!
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
